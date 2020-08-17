dd55f228ae3ec3535a23c62447125e1f35bb8606
lmh <lmh@weiyian.com>
2020-08-17 18:36:15 +0800

    init

---
+++ src/annotations/OpenService.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: lmh <lmh@weiyian.com>
+ * Date: 2020/8/17
+ * Time: 下午4:44
+ */
+
+namespace easyopen\annotations;
+
+
+use Doctrine\Common\Annotations\Annotation\Target;
+
+/**
+ * @Annotation
+ * @Target({"CLASS"})
+ */
+final class OpenService
+{
+
+}
