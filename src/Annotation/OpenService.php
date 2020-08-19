8a4961e6d5f3146e08545e735b3594da0d8c7ab5
lmh <lmh@weiyian.com>
2020-08-19 15:09:09 +0800

    init

--- src/Annotation/OpenService.php
+++ src/Annotation/OpenService.php
@@ -3,12 +3,11 @@ declare(strict_types=1);
 /**
  * Created by PhpStorm.
  * User: lmh <lmh@weiyian.com>
- * Date: 2020/8/17
- * Time: 下午4:44
+ * Date: 2020/8/19
+ * Time: 下午3:08
  */
 
-namespace easyopen\annotations;
-
+namespace lmh\easyopen\Annotation;
 
 use Doctrine\Common\Annotations\Annotation\Target;
 
