e1314f6f7565ee246e933e31a3b4d587dd53f676
lmh <lmh@weiyian.com>
2020-08-20 13:38:16 +0800

    init

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -27,7 +27,6 @@ class ConfigProvider
             ],
             'listeners' => [
                 OpenMappingListener::class,
-                OpenMappingCollector::class
             ],
             'annotations' => [
                 'scan' => [
